5edefe2a5ef38975e2f959ecf40c036de533f0ba
hyejoung <qkrgpwjd1996@gmail.com>
2025-06-27 14:33:44 +0900

    빌드 후 배포

---
+++ src/Todo.jsx
@@ -0,0 +1,17 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import './assets/styles/reset.css';
+import './assets/styles/Todo.css';
+
+function Todo(){
+    return (
+        <main>
+            <input type="text" placeholder= "오늘의 할일을 적어주세요" />
+            <button type="button">
+                <FontAwesomeIcon icon={faPenToSquare} />
+            </button>
+        </main>
+    )
+}
+
+export default Todo;
